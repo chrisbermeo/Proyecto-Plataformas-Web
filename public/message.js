28f9f4707633496ed5c7325b98889cbb046b0bfb
Steven Herrera <stev1998@hotmail.com>
2020-09-02 17:12:52 -0500

    Modificando archivos 1

--- public/message.js
+++ public/message.js
@@ -1,1 +1,15 @@
-console.log('Hola')
+function mostrar(){
+    let filtro= document.getElementById("filtro").value;
+    var tabla;
+    console.log(filtro)
+    fetch('http://localhost:3000/message?user='+filtro)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        for (data of myJson.body){
+            tabla+='<tr><td>'+data._id+'</td><td>'+data.user+'</td><td>'+data.message+'</td><td>'+data.date+'</td></tr>';
+        }
+        $('#tbody').html(tabla);
+    });
+}
